47c06416a67c09cf2d2b20beb834b5b9fe1d7bbd
amoody08 <135896148+amoody08@users.noreply.github.com>
2023-07-29 12:08:34 -0400

    Got capitalization correct; Code now correctly returns both entries

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,11 +1,23 @@
-let playerSelection;
-let computerSelection;
 let gameArray = ['Rock','Paper','Scissors'];
+let computerSelection;
+let playerSelection;
+let playerInput = prompt("Select 'Rock', 'Paper', or 'Scissors':");
 
 
      function getComputerChoice() {
-          let computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
+          computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
           return computerSelection;
+     }
+
+     function getPlayerChoice(playerInput) {
+          /*
+     for (let i = 0; i < playerInput.length; i++) {
+          playerInput[i] = playerInput[i][0].toUpperCase() + playerInput[i].toLowerCase(1);
+      }
+      */
+      playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
+      return playerSelection;
      }
      
 console.log(getComputerChoice())
+console.log(getPlayerChoice(playerInput))
