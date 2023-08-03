f69eaffe928b6a012da850ecb7e8d9aeb39e8908
amoody08 <135896148+amoody08@users.noreply.github.com>
2023-08-02 21:29:33 -0400

    Added comments to explain code

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,15 +5,18 @@ let winCount = 0;
 let lossCount = 0;
 
 
-for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) 
+for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++)
+// Loops the game until roundsPlayed = 5 
 {
 
      function getComputerChoice() {
           computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
           return computerSelection;
+//Chooses random item from the "gameArray" for the computer's choice
      }
      let playerInput = prompt("Select 'Rock', 'Paper', or 'Scissors':");
-          playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();    
+          playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();  
+//Fixes formatting to capitalize first letter of user's choice and make the rest of the word lower case  
 
      function playRound(playerSelection, computerSelection) {
           switch(playerSelection) {
@@ -21,6 +24,7 @@ for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++)
           switch(computerSelection) {
                case "Rock":
                     --roundsPlayed;
+//If there's a draw, roundsPlayed won't go up, so the game won't win until there have been 5 wins
                     return 'Draw! Play Again!';
                case "Paper":
                     ++lossCount;
@@ -55,6 +59,7 @@ for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++)
           }
      default:
           alert("Unknown Selection.  Please try again.")
+//If user types something other than "Rock", "Paper", or "Scissors", a dialog will pop up.
      }
 }
 
@@ -63,6 +68,7 @@ for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++)
 console.log("Computer choice: " + getComputerChoice());
 console.log("Player choice: " + playerSelection);
 console.log(playRound(playerSelection,computerSelection));
+//Displays Computer's choice, Player's choice, and runs program to see who wins.
 }
 
      function winTally(winCount,lossCount) {
@@ -72,7 +78,9 @@ console.log(playRound(playerSelection,computerSelection));
           else if (winCount < lossCount) {
                return "You lose!";
           }
+//Returns "You win" or "You lose" depending on who got the most wins.
      }
 
 console.log(winTally(winCount,lossCount));
 console.log("You won " + winCount + " rounds this game out of 5.");
+//Displays final score and how many games the user won.
