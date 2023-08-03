3091f7cf5df930a67f46d10e712cf7d2999f632d
amoody08 <135896148+amoody08@users.noreply.github.com>
2023-08-02 21:18:51 -0400

    Fixed tallying errors; Runs extra rounds for draws;

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,7 +5,8 @@ let winCount = 0;
 let lossCount = 0;
 
 
-for (let i = 0; i < 5; i++) {
+for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) 
+{
 
      function getComputerChoice() {
           computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
@@ -19,64 +20,59 @@ for (let i = 0; i < 5; i++) {
                case "Rock": 
           switch(computerSelection) {
                case "Rock":
+                    --roundsPlayed;
                     return 'Draw! Play Again!';
-                    break;
                case "Paper":
+                    ++lossCount;
                     return 'You lose!  Paper beats rock!';
-                    lossCount = lossCount++;
-                    break;
                case "Scissors":
+                    ++winCount;
                     return 'You win! Rock beats scissors!';
-                    winCount = winCount + 1;
-                    break;
           }
                case "Paper": 
           switch(computerSelection) {
                case "Paper":
+                    --roundsPlayed;
                     return 'Draw! Play Again!';
-                    break;
                case "Rock":
+                    ++winCount;
                     return 'You win!  Paper beats rock!';
-                    break;
-                    winCount = winCount + 1;
                case "Scissors":
+                    ++lossCount;
                     return 'You lose! Scissors beat paper!';
-                    lossCount++;
-                    break;
           }
                case "Scissors": 
           switch(computerSelection) {
                case "Scissors":
+                    --roundsPlayed;
                     return 'Draw! Play Again!';
-                    break;
                case "Paper":
+                    ++winCount
                     return 'You win!  Scissors beat paper!';
-                    break;
-                    winCount = winCount + 1;
                case "Rock":
+                    ++lossCount;
                     return 'You lose! Rock beats scissors!';
-                    lossCount++;
-                    break;
           }
      default:
           alert("Unknown Selection.  Please try again.")
      }
 }
 
+
+
 console.log("Computer choice: " + getComputerChoice());
 console.log("Player choice: " + playerSelection);
 console.log(playRound(playerSelection,computerSelection));
 }
-/*     function winTally(winCount,lossCount) {
+
+     function winTally(winCount,lossCount) {
           if (winCount > lossCount) {
                return "You win!";
           }
-     if (winCount < lossCount) {
+          else if (winCount < lossCount) {
                return "You lose!";
           }
      }
-*/
-//console.log(winTally());
-//console.log("You won " + winCount + " rounds this game out of 5.");
-console.log(winCount);
 
+console.log(winTally(winCount,lossCount));
+console.log("You won " + winCount + " rounds this game out of 5.");
